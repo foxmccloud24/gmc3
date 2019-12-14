61c4edec3773e7720a7c430ea7345a15247f307a
foxmccloud24 <56215296+foxmccloud24@users.noreply.github.com>
2019-12-14 05:16:18 -0800

    Add files via upload

---
+++ gmc/js/app.js
@@ -0,0 +1,50 @@
+
+
+
+alert(i);
+	return((a+b+c)/4);
+
+
+/*$("button").click(function(){
+	$("#first").fadeIn();
+	$("#second").fadeIn("slow");
+	$("#third").fadeIn(3000);
+});*/
+
+	
+
+	/*$("#main").click(function(){
+		$("#child").slideDown("slow");
+	});*/
+ 
+ /*var a=5
+ /*var i;
+ /*var myString="hello world"
+ /*var x=5
+ alert(a==b);
+ alert(a==c);*/
+ alert(myString.toLowerCase());
+ alert(parts);*/
+ alert(secondString.toUpperCase());*/
+ alert(x+y);*/
+ for(i=1;i<3;i++){
+ var b=2
+ var c=5
+ var parts=myString.split(",");
+ var secondString="world"
+ var y=3
+ var z=2
+ }*/
+/* var myString="hello there, this is a split text"
+/*$("#myPara").html("test")*/
+/*$("button").click(function(){
+/*function mean(a,b,c){
+/*prompt("comment ca va ?");*/
+/*var myArray=[1,2,3,4];
+/*var myArray=[1,2,3,4];
+alert(mean(3,5,7));*/
+alert(myArray[2]) //Returns 3*/
+console.log(myArray)*/
+var otherArray=["1","2","3"];
+}
+});
